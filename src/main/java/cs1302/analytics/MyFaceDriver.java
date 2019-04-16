27218e694da19f9f472bb97279d1d409d4131f14
Sidharth Nair <ssn47721@uga.edu>
2019-04-16 15:50:06 -0400

    new additions

---
+++ src/main/java/cs1302/analytics/MyFaceDriver.java
@@ -0,0 +1,23 @@
+package cs1302.analytics;
+
+import cs1302.myface.MyFace;
+import cs1302.myface.MyFaceUser;
+import java.util.*;
+public class MyFaceDriver
+{
+    public static void main(String[] args)
+        {
+            // generate myface users
+            MyFace socialNetwork = new MyFace(1000);
+            MyFaceUser[] users = socialNetwork.getUsers().stream().toArray(MyFaceUser[]::new);
+            System.out.printf("MyFace user count: %d\n", users.length);
+            
+// setup sorting algorithm
+            Comparator<Integer> c = Integer::compareTo;
+            Swapper<Integer> s = Swapper.getStandardSwapper();
+            Integer[] a = new Integer[] { 3, 6, 1, 32, 9, 2 };
+            Sort<Integer> sort = new BubbleSort<>(a, 0, a.length - 1, c, s);
+            sort.printStats();
+            sort.printSorted();
+        }
+}
